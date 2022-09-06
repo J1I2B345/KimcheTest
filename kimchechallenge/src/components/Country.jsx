b583f07039256f0e331d0007256040bee5e366b7
J1I2B345 <juan.bevilacqua@outlook.com>
2022-09-06 16:35:31 -0300

    fixing bug a search

--- kimchechallenge/src/components/Country.jsx
+++ kimchechallenge/src/components/Country.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import s from "./Country.module.css";
 
 const Country = ({ options, country }) => {
-	if (!Array.isArray(country.currency)) {
+	console.log(country.country);
+	if (!Array.isArray(country.currency) && country.currency) {
 		country.currency = country.currency.split(",");
 	}
 	if (options === "Language") {
@@ -14,23 +15,28 @@ const Country = ({ options, country }) => {
 						: ""}
 				</h2>
 				<div className={s.containerInt}>
-					<h3>
-						<span>{country.emoji}</span>
-						<span>{country.name}</span>
+					<h3 className={s.mg0}>
+						{" "}
+						<span className={s.emoji}>{country.emoji}</span>
+						<span className={s.name}>{country.name}</span>
 					</h3>
-					<h3>{country.continent.name}</h3>
-					<h4>
+
+					<h4 className={s.mg0}>{country.continent.name}</h4>
+
+					<h5 className={s.mg0}>
 						{" "}
-						<span>Capital:</span>
-						<span>{country.capital}</span>
-					</h4>
-					<h3>
+						<p>Capital:</p>
+						<span className={s.span}>{country.capital}</span>
+					</h5>
+					<h5 className={s.mg0}>
 						{" "}
-						<span>Currency</span>{" "}
-						{country.currency.map((e, i) => (
-							<span key={i}>{e}</span>
+						<p>Currency</p>{" "}
+						{country.currency?.map((e, i) => (
+							<span className={s.span} key={i}>
+								{e}
+							</span>
 						))}
-					</h3>
+					</h5>
 				</div>
 			</div>
 		);
@@ -44,7 +50,7 @@ const Country = ({ options, country }) => {
 						<span className={s.emoji}>{country.emoji}</span>
 						<span className={s.name}>{country.name}</span>
 					</h3>
-					<h5 className={`${s.languages} ${s.mg0}`}>
+					<h5 className={s.mg0}>
 						<p>Language</p>
 						{country.languages.length
 							? country.languages.map((e) => (
@@ -63,7 +69,7 @@ const Country = ({ options, country }) => {
 					<h5 className={s.mg0}>
 						{" "}
 						<p>Currency</p>{" "}
-						{country.currency.map((e, i) => (
+						{country.currency?.map((e, i) => (
 							<span className={s.span} key={i}>
 								{e}
 							</span>
